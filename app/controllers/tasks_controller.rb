400d7994ed738450288c97b37abdbec6da39525a
Ruby <Ruby@gmail.com>
2018-03-24 09:35:41 +0000

    second

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,18 @@
 class TasksController < ApplicationController
   def index
-    
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
-  end
-
-  def create
+    @message = Task.new
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
 end
