bdc271a66b22db0adc72a9b38585e1008815c4b8
Ruby <Ruby@gmail.com>
2018-03-27 13:58:37 +0000

    add status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    
   end
 
   def new
@@ -12,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    
   end
   
   def create
@@ -41,7 +43,7 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
+    
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -49,10 +51,13 @@ class TasksController < ApplicationController
   end
   
   private
-
+  
+  def set_message
+    @task = Task.find(params[:id])
+  end
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
   
